308637d1a263cad984c3f2295dc04641a2d9176b
b <b@bc>
2021-11-17 23:35:51 +0600

    initial commit

---
+++ src/com/advancedjava/LinkedListExample.java
@@ -0,0 +1,17 @@
+package com.advancedjava;
+import java.util.LinkedList;
+
+
+public class LinkedListExample {
+
+	public static void main(String[] args) {
+		LinkedList<String> ll = new LinkedList();
+		ll.add("A");
+		ll.add("B");
+		
+		ll.add(1, "C");
+
+		System.out.println(ll);
+	}
+
+}
